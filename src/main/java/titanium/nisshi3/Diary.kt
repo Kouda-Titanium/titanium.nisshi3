034103c5eca983eb3e6642f9a51041fc963bf3b2
Titanium <kouda@pplc.co>
2022-06-02 07:21:26 +0900

    Create Diary.kt

---
+++ src/main/java/titanium/nisshi3/Diary.kt
@@ -0,0 +1,28 @@
+package titanium.nisshi3
+
+
+// definition
+
+class Diary(val items: List<DiaryItem>)
+
+class Category
+
+sealed class DiaryItem(val day: Int, val category: Category) {
+    init {
+        require(day >= 1)
+    }
+}
+
+class Work(day: Int, category: Category, val description: String, val timeRanges: HourMinuteRangeList, val isRest: Boolean) : DiaryItem(day, category)
+class Comment(day: Int, category: Category, val label: String) : DiaryItem(day, category)
+
+
+// Utility
+
+val Diary.lastDay get() = items.maxOfOrNull { it.day } ?: 1
+
+val Diary.works get(): List<Work> = items.filterIsInstance<Work>()
+val Diary.comments get(): List<Comment> = items.filterIsInstance<Comment>()
+fun <T : DiaryItem> Iterable<T>.filterDay(day: Int) = this.filter { it.day == day }
+fun <T : DiaryItem> Iterable<T>.filterCategory(category: Category) = this.filter { it.category == category }
+fun Iterable<Work>.filterNonRests() = this.filter { !it.isRest }
