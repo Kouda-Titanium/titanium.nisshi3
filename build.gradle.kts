bb24db2ebd9c735203317683af0b4d7e8cff4321
Titanium <kouda@pplc.co>
2022-06-02 07:17:04 +0900

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    application
+    kotlin("jvm") version "1.5.20"
+    `maven-publish`
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8", "1.5.20"))
+    testImplementation("junit:junit:4.12")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("main") {
+            from(components["java"])
+            groupId = "titanium"
+            artifactId = "titanium.nisshi3"
+            version = "0.0.0"
+        }
+    }
+    repositories {
+        maven {
+            url = project.rootDir.resolve("maven").toURI()
+        }
+    }
+}
